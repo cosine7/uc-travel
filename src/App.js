8707dbbd4e2274d530db913f0defd33016fefeed
ChenXiao Liu <810196813@qq.com>
2023-03-18 13:11:10 +0800

    complete home page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { ReactComponent as Logo } from './logo.svg';
+import './App.scss';
+import { NavLink, Outlet } from 'react-router-dom';
+import { useCallback, useEffect, useState } from 'react';
+import Footer from './components/Footer';
 
+const items = [
+  { text: '首页', path: '' },
+  { text: '解决方案', path: 'solution' },
+  { text: '企业旅游', path: 'travel' },
+  { text: '关于我们', path: 'about' },
+  { text: '加入我们', path: 'join' },
+]
+console.log('hello');
 function App() {
+  const [navBarClass, setNavBarClass] = useState(null)
+
+  const onScroll = useCallback(() => {
+    let className = window.scrollY > 0 ? 'nav-bar-white' : null
+
+    if (className !== navBarClass) {
+      setNavBarClass(className)
+    }
+
+  }, [navBarClass])
+
+  useEffect(() => {
+    window.addEventListener('scroll', onScroll)
+    return () => window.removeEventListener('scroll', onScroll)
+  }, [onScroll])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav className={`nav ${navBarClass}`}>
+        <div className='left'>
+          <Logo className='logo' />
+          {items.map(item => (
+            <NavLink
+              key={item.path}
+              to={item.path}
+              className={({ isActive }) => `nav-item${isActive ? ' nav-item-active' : ''}`}
+            >
+              {item.text}
+            </NavLink>
+          ))}
+        </div>
+        <p className='tel'>全国服务热线: 400-109-0166</p>
+      </nav>
+      <Outlet />
+      <Footer />
+    </>
   );
 }
 
